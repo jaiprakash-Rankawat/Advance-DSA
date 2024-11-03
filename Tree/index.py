0958e78ea68d62fbb6eb025280ac513122ebf8e4
Jai Prakash Rankawat <jaiparkashrankawat2@gmail.com>
2024-11-03 22:29:07 +0530

    Tree

--- Tree/index.py
+++ Tree/index.py
@@ -61,6 +61,70 @@ class Tree:
             else:
                 self.searchElement(current.left, data)
 
+    def inorder(self):
+        print("Inorder : ", end=" ")
+        self.Inorder(self.root)
+        print()
+
+    def Inorder(self, current):
+        if current:
+            self.Inorder(current.left)
+            print(current.data, end=" ")
+            self.Inorder(current.right)
+
+    def preorder(self):
+        print("Preorder :", end=" ")
+        self.Preorder(self.root)
+        print()
+
+    def Preorder(self, current):
+        if current:
+            print(current.data, end=" ")
+            self.Preorder(current.left)
+            self.Preorder(current.right)
+
+    def postorder(self):
+        print("Postorder :", end=" ")
+        self.Postorder(self.root)
+        print()
+
+    def Postorder(self, current):
+        if current:
+            self.Postorder(current.left)
+            self.Postorder(current.right)
+            print(current.data, end=" ")
+
+    def delete(self, element):
+        # case1 : tree is empty
+        if self.root is None:
+            print("Tree is empty ")
+            return
+        # case 2 : tree is Not empty
+        self.delete_element(self.root, element)
+
+    def delete_element(self, current, data):
+        if current is None:
+            return current
+        elif current.data < data:
+            current.right = self.delete_element(current.right, data)
+        elif current.data > data:
+            current.left = self.delete_element(current.left, data)
+        else:
+            if current.left is None:
+                return current.right
+            if current.right is None:
+                return current.left
+
+            temp = self.minimum(current.right)
+            current.data = temp.data
+            current.right = self.delete_element(current.right, temp.data)
+        return current
+
+    def minimum(self, current):
+        while current.left:
+            current = current.left
+        return current
+
 
 obj = Tree()
 # 9,13,7,3,8,14,18,2
@@ -72,5 +136,20 @@ obj.insert(8)
 obj.insert(14)
 obj.insert(18)
 obj.insert(2)
-obj.serach(8)
+obj.delete(2)
+obj.delete(14)
+obj.delete(9)
+obj.inorder()
+obj.preorder()
+obj.postorder()
 # obj.info()
+
+
+# tree
+# insert
+# search
+# treaversal (inorder,preorder,postorder,levelorder)
+# delete
+
+
+# avl tree
